60d4e6d937f43e3f096fd31882384115c5d9ce3c
Erick Manrique <erickmanrique@Ericks-MacBook-Pro.local>
2016-06-03 20:20:01 -0400

    added firebase framework

--- OrderRight/LoginViewController.swift
+++ OrderRight/LoginViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FirebaseAuth
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    var ref:FIRDatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.ref = FIRDatabase.database().reference()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,9 +27,42 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func login(sender: AnyObject) {
-        let controller = storyboard!.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
-        presentViewController(controller, animated: true, completion: nil)
+//        let controller = storyboard!.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+//        presentViewController(controller, animated: true, completion: nil)
+        startfacebookLogin()
     }
 
+    func startfacebookLogin(){
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self) { (facebookResult:FBSDKLoginManagerLoginResult!, facebookError:NSError!) in
+            if facebookError != nil{
+                print("facebook login failed\(facebookError)")
+            }
+            else{
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("successfully login in with facebook \(accessToken)")
+                let credential = FIRFacebookAuthProvider.credentialWithAccessToken(accessToken)
+                FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
+                    if let user = user{
+                        var userValue:[String:AnyObject] = [
+                            "email": user.email ?? ""
+                        ]
+                        for profile in user.providerData {
+                            userValue["provider"] = profile.providerID ?? ""
+                            userValue["name"] = profile.displayName ?? ""
+                            userValue["photoUrl"] = profile.photoURL?.absoluteString ?? ""
+                        }
+                        
+//                        let key = self.ref.child("users").childByAutoId().key
+//                        let childUpdates = ["/users/\(key)": userValue]
+//                        self.ref.updateChildValues(childUpdates)
+                        self.ref.child("users").child("\(user.uid)").setValue(userValue)
+                        
+                    }
+                    
+                }
+            }
+        }
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-OrderRight/Pods-OrderRight-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OrderRight : NSObject
+@end
+@implementation PodsDummy_Pods_OrderRight
+@end
